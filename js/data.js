3be299c47cce960dcd434f2d672171ba615b7c49
JingleToast <evali9876@gmail.com>
2025-05-28 16:21:27 +0800

    blog list 切板完成

---
+++ js/data.js
@@ -0,0 +1,101 @@
+const blogAry = [
+    {
+        id: 1,
+        img_path: "",
+        post_date: "2024/10/11",
+        title: "React 入門不再迷惘：三步驟帶你上手核心概念",
+        tag: ["React","入門基礎"],
+        tag_button:[],
+        content: "你是否對 React 感到好奇，卻苦惱於該如何正式啟動學習之路？身為前端工程師兼職涯諮詢師，我常遇到同學問我：「React到底該怎麼入門？」為了幫大家減少摸索的時間，我整理出三個循序漸進的關鍵步驟，帶你更輕鬆地掌握 React核心概念，真正把理論應用在實際專案中。"
+    },
+    {
+        id: 2,
+        img_path: "",
+        post_date: "",
+        title: "",
+        tag: [],
+        tag_button: [],
+        content: ""
+    },
+    {
+        id: 3,
+        img_path: "",
+        post_date: "",
+        title: "",
+        tag: [],
+        tag_button: [],
+        content: ""
+    },
+    {
+        id: 4,
+        img_path: "",
+        post_date: "",
+        title: "",
+        tag: [],
+        tag_button: [],
+        content: ""
+    },
+    {
+        id: 5,
+        img_path: "",
+        post_date: "",
+        title: "",
+        tag: [],
+        tag_button: [],
+        content: ""
+    },
+    {
+        id: 6,
+        img_path: "",
+        post_date: "",
+        title: "",
+        tag: [],
+        tag_button: [],
+        content: ""
+    },
+    {
+        id: 7,
+        img_path: "",
+        post_date: "",
+        title: "",
+        tag: [],
+        tag_button: [],
+        content: ""
+    },
+    {
+        id: 8,
+        img_path: "",
+        post_date: "",
+        title: "",
+        tag: [],
+        tag_button: [],
+        content: ""
+    },
+    {
+        id: 9,
+        img_path: "",
+        post_date: "",
+        title: "",
+        tag: [],
+        tag_button: [],
+        content: ""
+    },
+    {
+        id: 10,
+        img_path: "",
+        post_date: "",
+        title: "",
+        tag: [],
+        tag_button: [],
+        content: ""
+    },
+    {
+        id: 11,
+        img_path: "",
+        post_date: "",
+        title: "",
+        tag: [],
+        tag_button: [],
+        content: ""
+    }
+];
